74f0f28910099d37f0052a88e29fc37b72247400
Uğur Çağlar Çalışkan <ugur.caglarc@gmail.com>
2019-06-24 01:37:04 +0300

    İlan Ekleme

--- backend/services/ilanlarService.js
+++ backend/services/ilanlarService.js
@@ -28,6 +28,7 @@ router.get('/select/otomobil',async(req,res)=>{
     var query = await ilan_schema.find({"ilan_catagory":"otomobil"})
     res.send(query);
 })
+
 router.get('/select/telefon',async(req,res)=>{
     var query = await ilan_schema.find({"ilan_catagory":"telefon"})
     res.send(query);
@@ -40,6 +41,50 @@ router.get('/select/diger',async(req,res)=>{
     var query = await ilan_schema.find({"ilan_catagory":"diger"})
     res.send(query);
 })
+
+router.post('/ekle',async(req, res)=>{
+    var new_ilan = new ilan_schema(request.body);
+    console.log(request.body)
+    new_user.save(err => {
+        if (err) {
+            console.log('İlan Eklerken Bir Hata Oluştur')
+            return response.status(500).send({ message: err })
+
+        }
+        console.log("İlan Ekleme Başarılı")
+        response.status(201).send({message:'İlan Ekleme Tamam'})
+    })
+})
+
+var multer  = require('multer');
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './images');
+    },
+    filename: (req, file, cb) => {
+      console.log(file);
+      var filetype = '';
+      if(file.mimetype === 'image/gif') {
+        filetype = 'gif';
+      }
+      if(file.mimetype === 'image/png') {
+        filetype = 'png';
+      }
+      if(file.mimetype === 'image/jpeg') {
+        filetype = 'jpg';
+      }
+      cb(null, 'image-' + Date.now() + '.' + filetype);
+    }
+});
+var upload = multer({storage: storage});
+router.post('/upload',upload.single('file'),function(req, res, next) {
+    console.log(req.file);
+    if(!req.file) {
+      res.status(500);
+      return next(err);
+    }
+    res.json({ fileUrl: 'http://localhost:8080/images/' + req.file.filename });
+  })
 
 var ilan = {router};
 
--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
@@ -13,6 +13,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { RegisterComponent } from './compenents/register/register.component';
 import { componentFactoryName } from '@angular/compiler';
 import { LoginComponent } from './compenents/login/login.component';
+import { NewIlanComponent } from './compenents/new-ilan/new-ilan.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { LoginComponent } from './compenents/login/login.component';
     NavComponent,
     SidebarComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    NewIlanComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +38,8 @@ import { LoginComponent } from './compenents/login/login.component';
       {path:'ev',component:IlanlarComponent},
       {path:'diger',component:IlanlarComponent},
       {path:'kayıt',component:RegisterComponent},
-      {path:'giris',component:LoginComponent}
+      {path:'giris',component:LoginComponent},
+      {path:'ilanekle',component:NewIlanComponent}
       
 
     ])
