f13afdd20692d978a4cf27d5280d8e9abd5f3523
Yusuf KALAY <ykalay1997@gmail.com>
2019-06-25 03:06:33 +0300

    alert ve ilan silme eklendi

--- frontend/angular/src/app/compenents/new-ilan/new-ilan.component.ts
+++ frontend/angular/src/app/compenents/new-ilan/new-ilan.component.ts
@@ -5,11 +5,8 @@ import { interpolation1 } from '@angular/core/src/render3';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { user } from '../register/user';
-<<<<<<< HEAD
 declare let alertify:any;
-=======
 import { Router } from '@angular/router';
->>>>>>> f3ff614e2a79f2f3b04b6e8a425855d689dec4b0
 
 import { __assign } from 'tslib';
 @Component({
@@ -28,18 +25,15 @@ export class NewIlanComponent implements OnInit {
   ngOnInit() {
     if(localStorage.getItem("user_info") == null)
     {
-<<<<<<< HEAD
       
       alertify.error("İlan Eklemek İçin Giriş Yapmalısınız")
       alertify.alert("İlan Eklemek İçin Giriş Yapmalısınız", function(){
     alertify.message('OK');
   });
   window.location.assign(this.path_home);
-=======
       /*window.location.assign(this.path_home);*/
       this.router.navigate(['/']);
       alert("İlan Eklemek İçin Giriş Yapmalısınız")
->>>>>>> f3ff614e2a79f2f3b04b6e8a425855d689dec4b0
       
     }
     this.ilanService.getuser_id(localStorage.getItem("user_info")).subscribe(data => {
